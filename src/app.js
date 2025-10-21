d5116a153b2623ba10e0e2a69d9e2198450ba6cb
JERF99 <jerf.2025.frej@gmail.com>
2025-10-20 21:45:58 -0600

    Se creo Router

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const app = express();
 
+const routes = require('./routes/index.routes');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Implementación de Rutas
+app.use('/', routes);
+
 app.listen(3000);
 console.log(`Server on Port: ${3000}`);
