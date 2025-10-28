ebf16fdced9ab1d1ab1178ff788b037bf553e307
JERF99 <jerf.2025.frej@gmail.com>
2025-10-27 21:49:30 -0600

    Avance estilo de HomePage

---
+++ public/javascript/loadBodyPrincipal.js
@@ -0,0 +1,6 @@
+fetch("/html/bodyPrincipal.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("bodyPrincipal-container").innerHTML = data;
+        document.dispatchEvent(new Event("footerCargado"));
+    });
